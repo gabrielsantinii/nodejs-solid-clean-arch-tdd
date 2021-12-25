63e986790a3972e2e962d4334f405ec3625f5f5e
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-25 19:24:24 -0300

    feat: domain usecase get-like-count-by-post-id added

--- src/data/usecases/get-like-count-by-post-id/get-like-count-by-post-id.spec.ts
+++ src/data/usecases/get-like-count-by-post-id/get-like-count-by-post-id.spec.ts
@@ -1,21 +1,21 @@
 class GetLikeCountByPostIdRepository {
-    countLikeByPostId({ postId }: { postId: string }) {
-        return 10
-    }
+  countLikeByPostId({ postId }: { postId: string }) {
+    return 10;
+  }
 }
 
 class GetLikeCountByPostId {
-    constructor(private readonly getLikeCountByPostIdRepository: GetLikeCountByPostIdRepository) {}
-    perform({ postId }: { postId: string }) {
-        return this.getLikeCountByPostIdRepository.countLikeByPostId({ postId })
-    }
+  constructor(private readonly getLikeCountByPostIdRepository: GetLikeCountByPostIdRepository) {}
+  perform({ postId }: { postId: string }) {
+    return this.getLikeCountByPostIdRepository.countLikeByPostId({ postId });
+  }
 }
 
 describe("get-like-count-by-post-id usecase", () => {
-    it("should return the count of likes in some post based on postId", async () => {
-        const getLikeCountByPostIdRepository = new GetLikeCountByPostIdRepository()
-        const sut = new GetLikeCountByPostId(getLikeCountByPostIdRepository)
-        const likeCount = sut.perform({ postId: "any_post_id" })
-        expect(typeof likeCount).toBe('number')
-    })
-})
+  it("should return the count of likes in some post based on postId", async () => {
+    const getLikeCountByPostIdRepository = new GetLikeCountByPostIdRepository();
+    const sut = new GetLikeCountByPostId(getLikeCountByPostIdRepository);
+    const likeCount = sut.perform({ postId: "any_post_id" });
+    expect(typeof likeCount).toBe("number");
+  });
+});
---
+++ src/domain/usecases/get-like-count-by-post-id.ts
@@ -0,0 +1,7 @@
+export interface GetLikeCountByPostId {
+  perform: (params: GetLikeCountByPostId.Params) => Promise<GetLikeCountByPostId.Result>;
+}
+export namespace GetLikeCountByPostId {
+  export type Params = { postId: string };
+  export type Result = number;
+}
