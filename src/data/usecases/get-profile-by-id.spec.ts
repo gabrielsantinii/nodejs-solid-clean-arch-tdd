b978ebeef83ec72d363fa0e8e2c4b98857a39468
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-25 17:34:51 -0300

    jest configured for ts, ensure get-profile-by-id returns an existing user

--- src/data/usecases/get-profile-by-id.spec.ts
+++ src/data/usecases/get-profile-by-id.spec.ts
@@ -1,5 +1,15 @@
+class GetProfileById {
+  perform({ profileId }: { profileId: string }) {
+    return {
+      id: profileId,
+    };
+  }
+}
+
 describe("get-profile-by-id usecase", () => {
   it("should return an existing profile", async () => {
-    expect(1).toBeTruthy();
+    const sut = new GetProfileById();
+    const profileData = sut.perform({ profileId: "any_profile_id" });
+    expect(profileData.id).toBe("any_profile_id");
   });
 });
