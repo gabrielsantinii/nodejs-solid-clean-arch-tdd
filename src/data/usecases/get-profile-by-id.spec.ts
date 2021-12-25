aec15fdb832023f65fdac15585d3e08e6e358ff1
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-25 17:37:45 -0300

    test: ensure get-profile-by-id returns undefined for unexisting profile

--- src/data/usecases/get-profile-by-id.spec.ts
+++ src/data/usecases/get-profile-by-id.spec.ts
@@ -1,5 +1,7 @@
 class GetProfileById {
-  perform({ profileId }: { profileId: string }) {
+  perform({ profileId }: { profileId: string }): { id: string } | undefined {
+    if (profileId !== "existant_id") return undefined;
+
     return {
       id: profileId,
     };
@@ -9,7 +11,13 @@ class GetProfileById {
 describe("get-profile-by-id usecase", () => {
   it("should return an existing profile", async () => {
     const sut = new GetProfileById();
+    const profileData = sut.perform({ profileId: "existant_id" });
+    expect(profileData?.id).toBe("existant_id");
+  });
+
+  it("should return undefined for not existing profile", async () => {
+    const sut = new GetProfileById();
     const profileData = sut.perform({ profileId: "any_profile_id" });
-    expect(profileData.id).toBe("any_profile_id");
+    expect(profileData?.id).toBeUndefined();
   });
 });
