c76f7ed8a99b998a93447934714f3cda6b28abb3
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-25 19:20:57 -0300

    test: get-like-count-by-post-id-repository added as dependency

--- src/data/usecases/get-like-count-by-post-id/get-like-count-by-post-id.spec.ts
+++ src/data/usecases/get-like-count-by-post-id/get-like-count-by-post-id.spec.ts
@@ -1,12 +1,20 @@
-class GetLikeCountByPostId {
-    perform({ postId }: { postId: string }) {
+class GetLikeCountByPostIdRepository {
+    countLikeByPostId({ postId }: { postId: string }) {
         return 10
+    }
+}
+
+class GetLikeCountByPostId {
+    constructor(private readonly getLikeCountByPostIdRepository: GetLikeCountByPostIdRepository) {}
+    perform({ postId }: { postId: string }) {
+        return this.getLikeCountByPostIdRepository.countLikeByPostId({ postId })
     }
 }
 
 describe("get-like-count-by-post-id usecase", () => {
     it("should return the count of likes in some post based on postId", async () => {
-        const sut = new GetLikeCountByPostId()
+        const getLikeCountByPostIdRepository = new GetLikeCountByPostIdRepository()
+        const sut = new GetLikeCountByPostId(getLikeCountByPostIdRepository)
         const likeCount = sut.perform({ postId: "any_post_id" })
         expect(typeof likeCount).toBe('number')
     })
