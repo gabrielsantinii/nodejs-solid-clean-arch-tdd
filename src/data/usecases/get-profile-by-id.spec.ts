745d7a76fd81953ab3fd0e30a40639bae270df97
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-25 17:50:36 -0300

    get-profile-by-id repository, sut factory

--- src/data/usecases/get-profile-by-id.spec.ts
+++ src/data/usecases/get-profile-by-id.spec.ts
@@ -7,31 +7,52 @@ export namespace GetProfileById {
     profileId: string;
   };
 
-  export type Model = {
-    id: string;
-  };
+  export type Model =
+    | {
+        id: string;
+      }
+    | undefined;
 }
 
-class DbGetProfileById {
-  perform({ profileId }: { profileId: string }): { id: string } | undefined {
-    if (profileId !== "existant_id") return undefined;
-
-    return {
-      id: profileId,
-    };
+class GetProfileByIdRepository {
+  async perform(profileId: string): Promise<{ id: string } | undefined> {
+    if (profileId === "existant_id") return { id: profileId };
+    
+    return undefined;
   }
 }
 
+class DbGetProfileById implements GetProfileById {
+  constructor(private readonly getProfileByIdRepository: GetProfileByIdRepository) {}
+
+  async perform({ profileId }: { profileId: string }) {
+    const profileData = await this.getProfileByIdRepository.perform(profileId);
+
+    return profileData;
+  }
+}
+
+type SutType = {
+  getProfileByIdRepository: GetProfileByIdRepository;
+  sut: DbGetProfileById;
+};
+
+const makeSut = (): SutType => {
+  const getProfileByIdRepository = new GetProfileByIdRepository();
+  const sut = new DbGetProfileById(getProfileByIdRepository);
+  return { sut, getProfileByIdRepository };
+};
+
 describe("get-profile-by-id usecase", () => {
   it("should return an existing profile", async () => {
-    const sut = new DbGetProfileById();
-    const profileData = sut.perform({ profileId: "existant_id" });
+    const { sut } = makeSut();
+    const profileData = await sut.perform({ profileId: "existant_id" });
     expect(profileData?.id).toBe("existant_id");
   });
 
   it("should return undefined for not existing profile", async () => {
-    const sut = new DbGetProfileById();
-    const profileData = sut.perform({ profileId: "any_profile_id" });
+    const { sut } = makeSut();
+    const profileData = await sut.perform({ profileId: "any_profile_id" });
     expect(profileData?.id).toBeUndefined();
   });
 });
