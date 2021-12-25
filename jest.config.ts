484b7ac14837f6fafce540d6980f5f110327b0b3
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-25 18:21:45 -0300

    chore: ts and jest mapping source config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,13 @@
 export default {
   clearMocks: true,
   coverageProvider: "v8",
-
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
   preset: "ts-jest",
   testMatch: ["**/**/*.spec.ts"],
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 };
