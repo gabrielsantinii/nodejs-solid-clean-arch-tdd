af7d4fc0a279c8450d276a46fec6baebc53832bf
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-25 17:40:42 -0300

    domain dependency inversion on get-profile-by-id

--- src/data/usecases/get-profile-by-id.spec.ts
+++ src/data/usecases/get-profile-by-id.spec.ts
@@ -1,4 +1,18 @@
-class GetProfileById {
+interface GetProfileById {
+  perform: (params: GetProfileById.Params) => Promise<GetProfileById.Model>;
+}
+
+export namespace GetProfileById {
+  export type Params = {
+    profileId: string;
+  };
+
+  export type Model = {
+    id: string;
+  };
+}
+
+class DbGetProfileById {
   perform({ profileId }: { profileId: string }): { id: string } | undefined {
     if (profileId !== "existant_id") return undefined;
 
@@ -10,13 +24,13 @@ class GetProfileById {
 
 describe("get-profile-by-id usecase", () => {
   it("should return an existing profile", async () => {
-    const sut = new GetProfileById();
+    const sut = new DbGetProfileById();
     const profileData = sut.perform({ profileId: "existant_id" });
     expect(profileData?.id).toBe("existant_id");
   });
 
   it("should return undefined for not existing profile", async () => {
-    const sut = new GetProfileById();
+    const sut = new DbGetProfileById();
     const profileData = sut.perform({ profileId: "any_profile_id" });
     expect(profileData?.id).toBeUndefined();
   });
