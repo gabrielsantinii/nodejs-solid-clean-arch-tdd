8775165cb4a548b968c5d111a5b300b2e760fe02
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-26 15:57:39 -0300

    test> load-recent-posts spy added as dependency to load-recent-posts-controller

--- src/presentation/controllers/load-recent-posts-controller/load-recent-posts-controller.spec.ts
+++ src/presentation/controllers/load-recent-posts-controller/load-recent-posts-controller.spec.ts
@@ -1,14 +1,25 @@
+import { LoadRecentPosts } from "@/domain/usecases";
 import { Controller, HttpResponse } from "@/presentation/protocols";
 
+class LoadRecentPostsSpy implements LoadRecentPosts {
+    async perform(params: LoadRecentPosts.Params): Promise<LoadRecentPosts.Result> {
+        return [];
+    }
+}
+
 class LoadRecentPostsController implements Controller {
+    constructor(private readonly loadRecentPosts: LoadRecentPosts) {}
+
     async handle(request: { followedBy: string }): Promise<HttpResponse> {
-        return { body: { posts: [] }, statusCode: 200 };
+        const recentPosts = await this.loadRecentPosts.perform({ authorsIds: [""], limit: 20 });
+        return { body: { posts: recentPosts }, statusCode: 200 };
     }
 }
 
 describe("load-recent-posts-controller.spec usecase", () => {
     it("should return a successful status response.", async () => {
-        const sut = new LoadRecentPostsController();
+        const loadRecentPostsSpy = new LoadRecentPostsSpy();
+        const sut = new LoadRecentPostsController(loadRecentPostsSpy);
         const recentPosts = await sut.handle({ followedBy: "any_profile_id" });
         expect(recentPosts.statusCode).toBe(200);
     });
