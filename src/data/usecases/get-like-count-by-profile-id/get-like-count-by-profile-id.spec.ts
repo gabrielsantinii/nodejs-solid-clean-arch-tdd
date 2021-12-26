c7fb94a81026d2a5b33b74bebd516baca132a0d2
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-25 22:07:19 -0300

    test: get-like-count-by-profile-id-repository

--- src/data/usecases/get-like-count-by-profile-id/get-like-count-by-profile-id.spec.ts
+++ src/data/usecases/get-like-count-by-profile-id/get-like-count-by-profile-id.spec.ts
@@ -1,12 +1,21 @@
-class GetLikeCountByProfileId {
-    async perform({ profileId }: { profileId: string }): Promise<number> {
+class GetLikeCountByProfileIdRepository {
+    async countByProfileId({ profileId }: { profileId: string }): Promise<number> {
         return 0;
+    }
+}
+
+class GetLikeCountByProfileId {
+    constructor(private readonly getLikeCountByProfileIdRepository: GetLikeCountByProfileIdRepository) {}
+
+    async perform({ profileId }: { profileId: string }): Promise<number> {
+        return this.getLikeCountByProfileIdRepository.countByProfileId({ profileId });
     }
 }
 
 describe("get-like-count-by-profile-id.spec usecase", () => {
     it("should return a numeric count", async () => {
-        const sut = new GetLikeCountByProfileId();
+        const getLikeCountByProfileIdRepository = new GetLikeCountByProfileIdRepository()
+        const sut = new GetLikeCountByProfileId(getLikeCountByProfileIdRepository);
         const likesCounts = await sut.perform({ profileId: "any_profile_id" });
         expect(typeof likesCounts).toBe("number");
     });
