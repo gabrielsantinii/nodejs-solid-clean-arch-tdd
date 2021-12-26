ec73fc20583feee8d608fa7b5dcd4d89f496ea1e
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-25 22:23:34 -0300

    feature: get-like-count-by-profile-id

---
+++ src/data/usecases/get-like-count-by-profile-id/index.ts
@@ -0,0 +1,1 @@
+export * from './get-like-count-by-profile-id'
--- src/infra/db/mongodb/like-mongo-repository.ts
+++ src/infra/db/mongodb/like-mongo-repository.ts
@@ -1,7 +1,11 @@
-import { GetLikeCountByPostIdRepository } from "@/data/protocols/db";
+import { GetLikeCountByPostIdRepository, GetLikeCountByProfileIdRepository } from "@/data/protocols/db";
 
-export class LikeMongoRepository implements GetLikeCountByPostIdRepository {
+export class LikeMongoRepository implements GetLikeCountByPostIdRepository, GetLikeCountByProfileIdRepository {
     async countLikeByPostId(params: GetLikeCountByPostIdRepository.Params): Promise<GetLikeCountByPostIdRepository.Result> {
+        return 0;
+    }
+
+    async countByProfileId(params: GetLikeCountByProfileIdRepository.Params): Promise<GetLikeCountByProfileIdRepository.Result> {
         return 0;
     }
 }
