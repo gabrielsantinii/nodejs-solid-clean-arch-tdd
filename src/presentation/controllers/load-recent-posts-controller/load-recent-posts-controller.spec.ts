d18ec36150420c7ba9ed03205fe05b8893bc1b3b
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-26 16:18:49 -0300

    test: params and result mocked on spy classes

--- src/presentation/controllers/load-recent-posts-controller/load-recent-posts-controller.spec.ts
+++ src/presentation/controllers/load-recent-posts-controller/load-recent-posts-controller.spec.ts
@@ -1,15 +1,26 @@
 import { LoadFollowingAuthorsList, LoadRecentPosts } from "@/domain/usecases";
+import { noContent, ok } from "@/presentation/helpers";
 import { Controller, HttpResponse } from "@/presentation/protocols";
+import { mockPostModel } from "@/tests/domain/mocks";
 
 class LoadFollowingAuthorsListSpy implements LoadFollowingAuthorsList {
+    followedBy = "";
+    result = ["any_author_id", "any_author_id2", "any_author_id3"];
     async perform(params: LoadFollowingAuthorsList.Params): Promise<LoadFollowingAuthorsList.Result> {
-        return [];
+        this.followedBy = params.followedBy;
+        return this.result;
     }
 }
 
 class LoadRecentPostsSpy implements LoadRecentPosts {
+    result = [mockPostModel(), mockPostModel(), mockPostModel()];
+    authorsIds: string[] = [];
+    limit = 0;
+
     async perform(params: LoadRecentPosts.Params): Promise<LoadRecentPosts.Result> {
-        return [];
+        this.authorsIds = params.authorsIds;
+        this.limit = params.limit;
+        return this.result;
     }
 }
 
@@ -19,9 +30,13 @@ class LoadRecentPostsController implements Controller {
     async handle(request: { followedBy: string }): Promise<HttpResponse> {
         const followingAuthorsList = await this.loadFollowingAuthorsList.perform({ followedBy: request.followedBy });
         const recentPosts = await this.loadRecentPosts.perform({ authorsIds: followingAuthorsList, limit: 20 });
-        return { body: { posts: recentPosts }, statusCode: 200 };
+        if (!recentPosts.length) return noContent();
+
+        return ok(recentPosts);
     }
 }
+
+
 
 describe("load-recent-posts-controller.spec usecase", () => {
     it("should return a successful status response.", async () => {
@@ -30,6 +45,5 @@ describe("load-recent-posts-controller.spec usecase", () => {
         const sut = new LoadRecentPostsController(loadFollowingAuthorsListSpy, loadRecentPostsSpy);
         const recentPosts = await sut.handle({ followedBy: "any_profile_id" });
         expect(recentPosts.statusCode).toBe(200);
-        expect(recentPosts.body).toHaveProperty('posts')
     });
 });
