d68866077291830bfa0e6f457dfe76878cd9c954
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-26 16:04:20 -0300

    test: load-following-authors-list spy added as dependency on lead-recent-posts-controller

--- src/presentation/controllers/load-recent-posts-controller/load-recent-posts-controller.spec.ts
+++ src/presentation/controllers/load-recent-posts-controller/load-recent-posts-controller.spec.ts
@@ -1,5 +1,11 @@
-import { LoadRecentPosts } from "@/domain/usecases";
+import { LoadFollowingAuthorsList, LoadRecentPosts } from "@/domain/usecases";
 import { Controller, HttpResponse } from "@/presentation/protocols";
+
+class LoadFollowingAuthorsListSpy implements LoadFollowingAuthorsList {
+    async perform(params: LoadFollowingAuthorsList.Params): Promise<LoadFollowingAuthorsList.Result> {
+        return [];
+    }
+}
 
 class LoadRecentPostsSpy implements LoadRecentPosts {
     async perform(params: LoadRecentPosts.Params): Promise<LoadRecentPosts.Result> {
@@ -8,18 +14,20 @@ class LoadRecentPostsSpy implements LoadRecentPosts {
 }
 
 class LoadRecentPostsController implements Controller {
-    constructor(private readonly loadRecentPosts: LoadRecentPosts) {}
+    constructor(private readonly loadFollowingAuthorsList: LoadFollowingAuthorsList, private readonly loadRecentPosts: LoadRecentPosts) {}
 
     async handle(request: { followedBy: string }): Promise<HttpResponse> {
-        const recentPosts = await this.loadRecentPosts.perform({ authorsIds: [""], limit: 20 });
+        const followingAuthorsList = await this.loadFollowingAuthorsList.perform({ followedBy: request.followedBy });
+        const recentPosts = await this.loadRecentPosts.perform({ authorsIds: followingAuthorsList, limit: 20 });
         return { body: { posts: recentPosts }, statusCode: 200 };
     }
 }
 
 describe("load-recent-posts-controller.spec usecase", () => {
     it("should return a successful status response.", async () => {
+        const loadFollowingAuthorsListSpy = new LoadFollowingAuthorsListSpy();
         const loadRecentPostsSpy = new LoadRecentPostsSpy();
-        const sut = new LoadRecentPostsController(loadRecentPostsSpy);
+        const sut = new LoadRecentPostsController(loadFollowingAuthorsListSpy, loadRecentPostsSpy);
         const recentPosts = await sut.handle({ followedBy: "any_profile_id" });
         expect(recentPosts.statusCode).toBe(200);
     });
