9ff48854b85dd894fec53d4ad1dd72ea3f5285f5
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-25 22:33:07 -0300

    test: sut factory for get-like-count-by-profile-id

--- src/data/usecases/get-like-count-by-profile-id/get-like-count-by-profile-id.spec.ts
+++ src/data/usecases/get-like-count-by-profile-id/get-like-count-by-profile-id.spec.ts
@@ -1,10 +1,22 @@
+import { GetLikeCountByProfileIdRepository } from "@/data/protocols/db";
+import { GetLikeCountByProfileId } from "@/domain/usecases";
 import { LikeMongoRepository } from "@/infra/db";
 import { DbGetLikeCountByProfileId } from "./get-like-count-by-profile-id";
 
+type SutType = {
+    sut: GetLikeCountByProfileId;
+    getLikeCountByProfileIdRepository: GetLikeCountByProfileIdRepository;
+};
+
+const makeSut = (): SutType => {
+    const getLikeCountByProfileIdRepository = new LikeMongoRepository();
+    const sut = new DbGetLikeCountByProfileId(getLikeCountByProfileIdRepository);
+    return { sut, getLikeCountByProfileIdRepository };
+};
+
 describe("get-like-count-by-profile-id.spec usecase", () => {
     it("should return a numeric count", async () => {
-        const getLikeCountByProfileIdRepository = new LikeMongoRepository();
-        const sut = new DbGetLikeCountByProfileId(getLikeCountByProfileIdRepository);
+        const { sut } = makeSut();
         const likesCounts = await sut.perform({ profileId: "any_profile_id" });
         expect(typeof likesCounts).toBe("number");
     });
