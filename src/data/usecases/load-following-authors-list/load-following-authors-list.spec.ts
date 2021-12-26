f9adccaa0af843589c819702498378cb57d37693
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-26 00:29:37 -0300

    test: load-following-authors-list repository added

--- src/data/usecases/load-following-authors-list/load-following-authors-list.spec.ts
+++ src/data/usecases/load-following-authors-list/load-following-authors-list.spec.ts
@@ -1,12 +1,23 @@
-class LoadFollowingAuthorsList {
-    async perform({ followedBy }: { followedBy: string }): Promise<string[]> {
+import { LoadFollowingAuthorsList } from "@/domain/usecases";
+
+class LoadFollowingAuthorsListRepository {
+    async loadFollowingAuthors(params: LoadFollowingAuthorsList.Params): Promise<LoadFollowingAuthorsList.Result> {
         return ["any_id", "any_name"];
+    }
+}
+
+class DbLoadFollowingAuthorsList implements LoadFollowingAuthorsList {
+    constructor(private readonly loadFollowingAuthorsListRepository: LoadFollowingAuthorsListRepository) {}
+
+    async perform(params: LoadFollowingAuthorsList.Params): Promise<LoadFollowingAuthorsList.Result> {
+        return this.loadFollowingAuthorsListRepository.loadFollowingAuthors({ followedBy: params.followedBy });
     }
 }
 
 describe("load-following-authors-list.spec usecase", () => {
     it("should return a list with following authors.", async () => {
-        const sut = new LoadFollowingAuthorsList();
+        const loadFollowingAuthorsListRepository = new LoadFollowingAuthorsListRepository()
+        const sut = new DbLoadFollowingAuthorsList(loadFollowingAuthorsListRepository);
         const followingAuthors = await sut.perform({ followedBy: "any_profile_id" });
         expect(Array.isArray(followingAuthors)).toBe(true);
         expect(followingAuthors.some((f) => typeof f !== "string")).toBe(false);
