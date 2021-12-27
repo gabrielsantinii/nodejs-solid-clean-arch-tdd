26bd99eb7437a9396186f68368a32bb8575b20fd
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-26 21:17:41 -0300

    chore: babel config for module aliases

--- src/infra/db/mongodb/post-mongo-repository.ts
+++ src/infra/db/mongodb/post-mongo-repository.ts
@@ -1,8 +1,7 @@
 import { LoadRecentPostsRepository } from "@/data/protocols/db";
-import { mockPostModel } from "@/tests/domain/mocks";
 
 export class PostMongoRepository implements LoadRecentPostsRepository {
     async loadWithLimit(params: LoadRecentPostsRepository.Params): Promise<LoadRecentPostsRepository.Result> {
-        return [mockPostModel(), mockPostModel()];
+        return [];
     }
 }
--- src/infra/db/mongodb/profile-mongo-repository.ts
+++ src/infra/db/mongodb/profile-mongo-repository.ts
@@ -1,10 +1,7 @@
 import { LoadProfileRepository } from "@/data/protocols/db";
-import { mockProfileModel } from "@/tests/domain/mocks/mock-profile";
 
 export class ProfileMongoRepository implements LoadProfileRepository {
     async loadProfile({ profileId }: LoadProfileRepository.Params): Promise<LoadProfileRepository.Result> {
-        if (profileId === "existant_id") return { ...mockProfileModel() };
-
         return undefined;
     }
 }
