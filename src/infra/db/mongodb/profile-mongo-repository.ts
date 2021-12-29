ca5c652623323d9532510bb011ccc31b86c8240b
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-29 14:02:38 -0300

    feat: implement add-profile on infra layer

--- src/infra/db/mongodb/profile-mongo-repository.ts
+++ src/infra/db/mongodb/profile-mongo-repository.ts
@@ -1,7 +1,11 @@
-import { LoadProfileRepository } from "@/data/protocols/db";
+import { AddProfileRepository, LoadProfileRepository } from "@/data/protocols/db";
 
-export class ProfileMongoRepository implements LoadProfileRepository {
+export class ProfileMongoRepository implements LoadProfileRepository, AddProfileRepository {
     async loadProfile({ profileId }: LoadProfileRepository.Params): Promise<LoadProfileRepository.Result> {
         return undefined;
+    }
+
+    async add(params: AddProfileRepository.Params): Promise<AddProfileRepository.Result> {
+        return;
     }
 }
