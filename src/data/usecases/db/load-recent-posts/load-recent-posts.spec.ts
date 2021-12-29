44e41d3f43fdf07a2b1999e72079fe2602000377
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-29 12:52:05 -0300

    test: load-recent-posts spy added

--- src/data/usecases/db/load-recent-posts/load-recent-posts.spec.ts
+++ src/data/usecases/db/load-recent-posts/load-recent-posts.spec.ts
@@ -1,18 +1,25 @@
 import { LoadRecentPostsRepository } from "@/data/protocols/db";
 import { LoadRecentPosts } from "@/domain/usecases";
-import { PostMongoRepository } from "@/infra/db";
 import { DbLoadRecentPosts } from "@/data/usecases/db";
+import { mockPostModel } from "@/tests/domain/mocks";
+
+class LoadRecentPostsRepositorySpy implements LoadRecentPostsRepository {
+    result: LoadRecentPostsRepository.Result = [mockPostModel(), mockPostModel()];
+    async loadWithLimit(params: LoadRecentPosts.Params): Promise<LoadRecentPosts.Result> {
+        return this.result;
+    }
+}
 
 type SutType = {
     sut: LoadRecentPosts;
-    loadRecentPostsRepository: LoadRecentPostsRepository;
+    loadRecentPostsRepositorySpy: LoadRecentPostsRepositorySpy;
 };
 
 const makeSut = (): SutType => {
-    const loadRecentPostsRepository = new PostMongoRepository();
-    const sut = new DbLoadRecentPosts(loadRecentPostsRepository);
+    const loadRecentPostsRepositorySpy = new LoadRecentPostsRepositorySpy();
+    const sut = new DbLoadRecentPosts(loadRecentPostsRepositorySpy);
 
-    return { sut, loadRecentPostsRepository };
+    return { sut, loadRecentPostsRepositorySpy };
 };
 
 describe("load-recent-posts.spec usecase", () => {
