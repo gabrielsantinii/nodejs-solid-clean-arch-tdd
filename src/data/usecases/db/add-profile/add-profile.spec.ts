6575210a6873281bd4cf90a9d5a342fcfbd8c2a9
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-29 13:48:45 -0300

    test: refactor sut to makeSut factory

--- src/data/usecases/db/add-profile/add-profile.spec.ts
+++ src/data/usecases/db/add-profile/add-profile.spec.ts
@@ -3,7 +3,16 @@ import { mockProfileModel } from "@/tests/domain/mocks";
 
 const mockProfileParams = (): AddProfile.Params => ({ name: "Ronaldo", email: "", password: "", username: "", description: "" });
 
-class AddProfileRepository {
+export interface AddProfileRepository {
+    add: (params: AddProfileRepository.Params) => Promise<AddProfileRepository.Result>;
+}
+
+export namespace AddProfileRepository {
+    export type Params = AddProfile.Params;
+    export type Result = AddProfile.Result;
+}
+
+class AddProfileRepositorySpy {
     result: AddProfile.Result = mockProfileModel();
     async add(params: AddProfile.Params): Promise<AddProfile.Result> {
         return this.result;
@@ -18,13 +27,29 @@ class DbAddProfile implements AddProfile {
     }
 }
 
+type SutType = {
+    addProfileRepositorySpy: AddProfileRepositorySpy;
+    sut: DbAddProfile;
+};
+
+const makeSut = (): SutType => {
+    const addProfileRepositorySpy = new AddProfileRepositorySpy();
+    const sut = new DbAddProfile(addProfileRepositorySpy);
+
+    return { addProfileRepositorySpy, sut };
+};
+
 describe("add-profile.spec usecase", () => {
     it("should create the profile and return the created data with id.", async () => {
-        const addProfileRepository = new AddProfileRepository();
-        const sut = new DbAddProfile(addProfileRepository);
+        const { sut } = makeSut();
         const createdProfile = await sut.perform(mockProfileParams());
         expect(createdProfile).toHaveProperty("id");
     });
 
-    // it("Should throws an error if the created profile do not provide the id.");
+    it("Should throws an error if the created profile do not provide the id.", async () => {
+        const { sut, addProfileRepositorySpy } = makeSut();
+
+        const createdProfile = await sut.perform(mockProfileParams());
+        expect(createdProfile).toHaveProperty("id");
+    });
 });
