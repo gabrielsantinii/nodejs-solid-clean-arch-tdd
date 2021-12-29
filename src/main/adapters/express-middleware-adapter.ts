ec369bb8677ccc327ec49716ba5171cf3c18ab7f
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-29 12:59:54 -0300

    feat: middleware adapt router

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { Middleware } from "@/presentation/protocols";
+import { isErrorStatus, safeSpread } from "../helpers";
+
+export const adaptMiddleware = (middleware: Middleware) => {
+    return async (expressRequest: express.Request, expressResponse: express.Response, nextFunction: express.NextFunction) => {
+        const request = { ...safeSpread(expressRequest.body), ...safeSpread(expressRequest.params), ...safeSpread(expressRequest.headers) };
+        const httpResponse = await middleware.handle(request);
+
+        if (isErrorStatus(httpResponse.statusCode)) {
+            return expressResponse.json({ errors: httpResponse.body.map((e: Error) => e.message) });
+        }
+
+        Object.assign(expressRequest, httpResponse.body);
+        nextFunction();
+    };
+};
