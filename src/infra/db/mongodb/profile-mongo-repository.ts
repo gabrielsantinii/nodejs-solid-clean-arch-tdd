7028c61a858a1dfb2e5b2d07bf32bea198dbc3a5
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-29 14:49:59 -0300

    infra: profile-mongo-repository implements check-profile-by-username protocol

--- src/infra/db/mongodb/profile-mongo-repository.ts
+++ src/infra/db/mongodb/profile-mongo-repository.ts
@@ -1,11 +1,15 @@
-import { AddProfileRepository, LoadProfileRepository } from "@/data/protocols/db";
+import { AddProfileRepository, CheckProfileByUsernameRepository, LoadProfileRepository } from "@/data/protocols/db";
 
-export class ProfileMongoRepository implements LoadProfileRepository, AddProfileRepository {
+export class ProfileMongoRepository implements LoadProfileRepository, AddProfileRepository, CheckProfileByUsernameRepository {
     async loadProfile({ profileId }: LoadProfileRepository.Params): Promise<LoadProfileRepository.Result> {
         return undefined;
     }
 
     async add(params: AddProfileRepository.Params): Promise<AddProfileRepository.Result> {
+        return;
+    }
+
+    async checkByUsername(params: CheckProfileByUsernameRepository.Params): Promise<CheckProfileByUsernameRepository.Result> {
         return;
     }
 }
