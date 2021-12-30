dc29da77403e7027f726d6593cef9376dde658a0
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-30 11:21:52 -0300

    test: ensure load-profile-with-posts-controller returns bad request on non-existing profileId

---
+++ src/presentation/controllers/load-profile-with-posts-controller/load-profile-with-posts-controller.spec.ts
@@ -0,0 +1,17 @@
+import { CustomParamError } from "@/presentation/errors";
+import { httpResponse } from "@/presentation/helpers";
+import { Controller, HttpResponse } from "@/presentation/protocols";
+
+class LoadProfileWithPostsController implements Controller {
+    async handle(request: any): Promise<HttpResponse> {
+        return httpResponse.badRequest([new CustomParamError(`Profile with id ${request.profileId} not found.`)]);
+    }
+}
+
+describe("load-profile-with-posts-controller.spec usecase", () => {
+    it("should return badRequest for non-existing profile.", async () => {
+        const sut = new LoadProfileWithPostsController();
+        const controllerResponse = await sut.handle({ profileId: 'any_prof_id' })
+        expect(controllerResponse.statusCode).toBe(400)
+    });
+});
