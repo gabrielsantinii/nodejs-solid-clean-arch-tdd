690715316d32c3d948d0a5694c36c579d3fa855d
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-30 20:46:24 -0300

    feat: id field validation middleware added to profiles routes

--- src/main/routes/profiles-routes/profiles-routes.ts
+++ src/main/routes/profiles-routes/profiles-routes.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { adaptMiddleware, adaptRoute } from "@/main/adapters";
 
-import { makeAddProfileValidationMiddleware } from "@/main/factories/middlewares";
+import { makeAddProfileValidationMiddleware, makeIdFieldValidationMiddleware } from "@/main/factories/middlewares";
 import { makeAddProfileController, makeLoadProfileController } from "@/main/factories/controllers";
 
 export const profileRoutes = (router: express.Router): void => {
-    router.post("/profiles", adaptMiddleware(makeAddProfileValidationMiddleware()), adaptRoute(makeAddProfileController()));
-    router.get("/profiles/:profileId", adaptRoute(makeLoadProfileController()));
+    router.post("/profiles", [adaptMiddleware(makeAddProfileValidationMiddleware()), adaptRoute(makeAddProfileController())]);
+    router.get("/profiles/:profileId", [
+        adaptMiddleware(makeIdFieldValidationMiddleware("profileId")),
+        adaptRoute(makeLoadProfileController()),
+    ]);
 };
