ad6d358184c1d7580905dd8aaf2ad4e6dfdc72c9
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-30 11:12:15 -0300

    infra: post-mongo-repository implements load-posts-by-author-repository protocol

--- src/infra/db/mongodb/post-mongo-repository.ts
+++ src/infra/db/mongodb/post-mongo-repository.ts
@@ -1,7 +1,11 @@
-import { LoadRecentPostsRepository } from "@/data/protocols/db";
+import { LoadPostsByAuthorRepository, LoadRecentPostsRepository } from "@/data/protocols/db";
 
-export class PostMongoRepository implements LoadRecentPostsRepository {
+export class PostMongoRepository implements LoadRecentPostsRepository, LoadPostsByAuthorRepository {
     async loadWithLimit(params: LoadRecentPostsRepository.Params): Promise<LoadRecentPostsRepository.Result> {
+        return [];
+    }
+
+    async loadByAuthor(params: LoadPostsByAuthorRepository.Params): Promise<LoadPostsByAuthorRepository.Result> {
         return [];
     }
 }
