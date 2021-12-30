a05f67b4bea9b53a2fb289e7e74a7d9f27273a4d
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-30 13:27:28 -0300

    profile-likes-count usecase added as dependency

--- src/presentation/controllers/load-profile-with-posts-controller/load-profile-with-posts-controller.spec.ts
+++ src/presentation/controllers/load-profile-with-posts-controller/load-profile-with-posts-controller.spec.ts
@@ -1,18 +1,23 @@
-import { LoadPostsByAuthor, LoadProfile } from "@/domain/usecases";
+import { CountProfileLikes, LoadPostsByAuthor, LoadProfile } from "@/domain/usecases";
 import { CustomParamError } from "@/presentation/errors";
 import { httpResponse } from "@/presentation/helpers";
 import { Controller, HttpResponse } from "@/presentation/protocols";
 import { mockPostModel, mockProfileModel } from "@/tests/domain/mocks";
 
 class LoadProfileWithPostsController implements Controller {
-    constructor(private readonly loadProfile: LoadProfile, private readonly loadPostsByAuthor: LoadPostsByAuthor) {}
+    constructor(
+        private readonly loadProfile: LoadProfile,
+        private readonly loadPostsByAuthor: LoadPostsByAuthor,
+        private readonly countProfileLikes: CountProfileLikes
+    ) {}
 
     async handle(request: any): Promise<HttpResponse> {
         const profile = await this.loadProfile.perform({ profileId: request.profileId });
         if (!profile) return httpResponse.badRequest([new CustomParamError(`Profile with id ${request.profileId} not found.`)]);
 
         const posts = await this.loadPostsByAuthor.perform({ authorId: request.profileId });
-        return httpResponse.ok({ ...profile, posts });
+        const profileLikesCount = await this.countProfileLikes.perform({ profileId: request.profileId });
+        return httpResponse.ok({ ...profile, posts, likesCount: profileLikesCount });
     }
 }
 
@@ -30,17 +35,26 @@ class LoadPostsByAuthorSpy implements LoadPostsByAuthor {
     }
 }
 
+class CountProfileLikesSpy implements CountProfileLikes {
+    result: CountProfileLikes.Result = 0;
+    async perform(params: CountProfileLikes.Params): Promise<CountProfileLikes.Result> {
+        return this.result;
+    }
+}
+
 type SutType = {
     sut: LoadProfileWithPostsController;
     loadProfileSpy: LoadProfileSpy;
     loadPostsByAuthorSpy: LoadPostsByAuthorSpy;
+    countProfileLikesSpy: CountProfileLikesSpy;
 };
 
 const makeSut = (): SutType => {
     const loadProfileSpy = new LoadProfileSpy();
     const loadPostsByAuthorSpy = new LoadPostsByAuthorSpy();
-    const sut = new LoadProfileWithPostsController(loadProfileSpy, loadPostsByAuthorSpy);
-    return { sut, loadProfileSpy, loadPostsByAuthorSpy };
+    const countProfileLikesSpy = new CountProfileLikesSpy();
+    const sut = new LoadProfileWithPostsController(loadProfileSpy, loadPostsByAuthorSpy, countProfileLikesSpy);
+    return { sut, loadProfileSpy, loadPostsByAuthorSpy, countProfileLikesSpy };
 };
 
 describe("load-profile-with-posts-controller.spec usecase", () => {
@@ -51,13 +65,15 @@ describe("load-profile-with-posts-controller.spec usecase", () => {
     });
 
     it("should return ok for existing profile.", async () => {
-        const { sut, loadProfileSpy, loadPostsByAuthorSpy } = makeSut();
+        const { sut, loadProfileSpy, loadPostsByAuthorSpy, countProfileLikesSpy } = makeSut();
         const mockProfile = mockProfileModel();
         const mockPost = mockPostModel();
+        const mockProfileLikesCount = 10;
         loadProfileSpy.result = mockProfile;
-        loadPostsByAuthorSpy.result = [mockPost]
+        loadPostsByAuthorSpy.result = [mockPost];
+        countProfileLikesSpy.result = mockProfileLikesCount;
         const controllerResponse = await sut.handle({ profileId: "any_prof_id" });
 
-        expect(controllerResponse).toEqual(httpResponse.ok({ ...mockProfile, posts: [mockPost] }));
+        expect(controllerResponse).toEqual(httpResponse.ok({ ...mockProfile, posts: [mockPost], likesCount: mockProfileLikesCount }));
     });
 });
