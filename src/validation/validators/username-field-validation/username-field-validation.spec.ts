c5e4c966898d481203cab02a36b250d99e35acfc
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-31 18:25:18 -0300

    feat: username-field-validation

--- src/validation/validators/username-field-validation/username-field-validation.spec.ts
+++ src/validation/validators/username-field-validation/username-field-validation.spec.ts
@@ -1,34 +1,5 @@
 import { InvalidParamError } from "@/presentation/errors";
-import { Validation } from "@/presentation/protocols";
-
-class UsernameFieldValidation implements Validation {
-    private readonly transformations: Array<(v: string) => string> = [
-        (v) => v.toLowerCase(),
-        // Remove all accents
-        (v) => v.normalize("NFD").replace(/\p{Diacritic}/gu, ""),
-    ];
-    private readonly validations: Array<(v: string) => boolean> = [
-        // Only alphanumeric characters
-        (v) => !!v.match(/^[a-z0-9_]/g),
-        (v) => v[0] !== "_",
-        (v) => !v.includes("__"),
-        (v) => !v.includes("@"),
-    ];
-    constructor(private readonly fieldName: string) {}
-
-    validate(input: any): Error {
-        const usernameToValidate = input[this.fieldName];
-        let transformedUsername = String(usernameToValidate);
-        for (const transformation of this.transformations) {
-            transformedUsername = transformation(transformedUsername);
-        }
-        for (const validation of this.validations) {
-            if (!validation(transformedUsername)) {
-                return new InvalidParamError(this.fieldName);
-            }
-        }
-    }
-}
+import { UsernameFieldValidation } from "@/validation/validators";
 
 describe("username-field-validation.spec usecase", () => {
     it("should return error for username that inits with '.'", async () => {
---
+++ src/validation/validators/username-field-validation/username-field-validation.ts
@@ -0,0 +1,31 @@
+import { InvalidParamError } from "@/presentation/errors";
+import { Validation } from "@/presentation/protocols";
+
+export class UsernameFieldValidation implements Validation {
+    private readonly transformations: Array<(v: string) => string> = [
+        (v) => v.toLowerCase(),
+        // Remove all accents
+        (v) => v.normalize("NFD").replace(/\p{Diacritic}/gu, ""),
+    ];
+    private readonly validations: Array<(v: string) => boolean> = [
+        // Only alphanumeric characters
+        (v) => !!v.match(/^[a-z0-9_]/g),
+        (v) => v[0] !== "_",
+        (v) => !v.includes("__"),
+        (v) => !v.includes("@"),
+    ];
+    constructor(private readonly fieldName: string) {}
+
+    validate(input: any): Error {
+        const usernameToValidate = input[this.fieldName];
+        let transformedUsername = String(usernameToValidate);
+        for (const transformation of this.transformations) {
+            transformedUsername = transformation(transformedUsername);
+        }
+        for (const validation of this.validations) {
+            if (!validation(transformedUsername)) {
+                return new InvalidParamError(this.fieldName);
+            }
+        }
+    }
+}
