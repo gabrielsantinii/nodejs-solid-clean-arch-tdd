2b1d6bf78d85bda1e10085c4eac35e32e069c557
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-31 17:16:35 -0300

    refactor: email-field-validation variable names refactored

--- src/validation/validators/email-field-validation/email-field-validation.ts
+++ src/validation/validators/email-field-validation/email-field-validation.ts
@@ -2,13 +2,13 @@ import { InvalidParamError } from "@/presentation/errors";
 import { Validation } from "@/presentation/protocols";
 
 export class EmailFieldValidation implements Validation {
-    private readonly regExp =
+    private readonly validEmailPattern =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     constructor(private readonly fieldName: string) {}
 
     validate(input: any): Error {
-        const val = String(input[this.fieldName]);
-        if (!val.match(this.regExp)) return new InvalidParamError(this.fieldName);
+        const emailToValidate = String(input[this.fieldName]);
+        if (!emailToValidate.match(this.validEmailPattern)) return new InvalidParamError(this.fieldName);
     }
 }
