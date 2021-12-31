5a30fad1a1753e4660723c6a92b1c480896b671a
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-30 21:21:31 -0300

    feat: email-field-validation

--- src/validation/validators/email-field-validation/email-field-validation.spec.ts
+++ src/validation/validators/email-field-validation/email-field-validation.spec.ts
@@ -1,17 +1,5 @@
 import { InvalidParamError } from "@/presentation/errors";
-import { Validation } from "@/presentation/protocols";
-
-class EmailFieldValidation implements Validation {
-    private readonly regExp =
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    constructor(private readonly fieldName: string) {}
-
-    validate(input: any): Error {
-        const val = String(input[this.fieldName]);
-        if (!val.match(this.regExp)) return new InvalidParamError(this.fieldName);
-    }
-}
+import { EmailFieldValidation } from "@/validation/validators";
 
 type SutType = {
     sut: EmailFieldValidation;
--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,3 +1,4 @@
 export * from './required-field-validation'
 export * from './length-field-validation'
 export * from './id-field-validation'
+export * from './email-field-validation'
