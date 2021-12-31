2a14a3b914064af4ad054ef6e0527e73ee2756ea
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-31 19:00:33 -0300

    test: add-auth-adapter interface created, add-auth-adapter-spy added to create sut

--- src/data/usecases/remote/add-auth/add-auth.spec.ts
+++ src/data/usecases/remote/add-auth/add-auth.spec.ts
@@ -1,8 +1,21 @@
+interface AddAuthAdapter {
+    add: (params: { email: string; password: string }) => Promise<{ authId: string }>;
+}
+
+class AddAuthAdapterSpy implements AddAuthAdapter {
+    result: { authId: string } = { authId: 'any_auth_id' }
+    async add(params: { email: string; password: string }): Promise<{ authId: string }> {
+        return this.result
+    }
+}
+
 interface AddAuth {
     perform: (params: { email: string; password: string }) => Promise<{ authId: string }>;
 }
 
 class RemoteAddAuth implements AddAuth {
+    constructor(private readonly addAuthAdapter: AddAuthAdapter) {}
+
     async perform(params: { email: string; password: string }): Promise<{ authId: string }> {
         const createdAuth = { authId: "any-auth-id" };
         return createdAuth;
@@ -11,7 +24,8 @@ class RemoteAddAuth implements AddAuth {
 
 describe("add-auth.spec usecase", () => {
     it("should return authId for valid email and password.", async () => {
-        const sut = new RemoteAddAuth();
+        const addAuthAdapterSpy = new AddAuthAdapterSpy()
+        const sut = new RemoteAddAuth(addAuthAdapterSpy);
         const createdAuth = await sut.perform({ email: "any_email@foo.com", password: "any_valid_pass" });
         expect(createdAuth).toHaveProperty("authId");
     });
