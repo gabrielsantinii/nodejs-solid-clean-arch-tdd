0f07c3ae391ac69bf2d8bc0aef4c86dbcb21f2c4
Gabriel Santini <gabrielgsantini@gmail.com>
2022-01-01 19:52:13 -0300

    feat: firebase-auth implements load-auth-id-by-auth-token-adapter

--- src/infra/remote/firebase/firebase-auth.ts
+++ src/infra/remote/firebase/firebase-auth.ts
@@ -1,10 +1,15 @@
 import firebaseAdmin from "firebase-admin";
-import { AddAuthAdapter } from "@/data/protocols/remote";
+import { AddAuthAdapter, LoadAuthIdByAuthTokenAdapter } from "@/data/protocols/remote";
 
-export class FirebaseAuth implements AddAuthAdapter {
+export class FirebaseAuth implements AddAuthAdapter, LoadAuthIdByAuthTokenAdapter {
     async add(params: AddAuthAdapter.Params): Promise<AddAuthAdapter.Result> {
         const createdAuth = await firebaseAdmin.auth().createUser({ email: params.email, password: params.password, uid: params.authId });
         return { authId: createdAuth.uid };
+    }
+
+    async loadAuthIdByToken(params: LoadAuthIdByAuthTokenAdapter.Params): Promise<LoadAuthIdByAuthTokenAdapter.Result> {
+        const auth = await firebaseAdmin.auth().verifyIdToken(params.token);
+        return { authId: auth.uid };
     }
 
     async delete(params: { authId: string }): Promise<void> {
