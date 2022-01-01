dc1616dc7fdf9051dca482f6da971505fcd7f238
Gabriel Santini <gabrielgsantini@gmail.com>
2022-01-01 18:47:41 -0300

    test: ensure return notAuthorized for authorization that hasnt Bearer prefix

--- src/presentation/middlewares/auth-middleware/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware/auth-middleware.spec.ts
@@ -3,15 +3,32 @@ import { HttpResponse, Middleware } from "@/presentation/protocols";
 
 class AuthMiddleware implements Middleware {
     async handle(request: { Authorization: string }): Promise<HttpResponse> {
-        const authorization = request?.Authorization
+        const authorization = request?.Authorization;
         if (!authorization) return httpResponse.notAuthorized();
+        const isValidToken = this.validateBearerTokenType(authorization);
+        if (!isValidToken.isValid) return httpResponse.notAuthorized();
+    }
+
+    private validateBearerTokenType(authorization: string): { isValid: boolean } {
+        const [prefix, token] = authorization.split(" ");
+        if (prefix !== "Bearer") return { isValid: false };
+        if (!token) return { isValid: false };
+        if (authorization.split(" ").length > 2) return { isValid: false };
+
+        return { isValid: true };
     }
 }
 
 describe("auth-middleware.spec usecase", () => {
-    it("should return ok with profile data.", async () => {
+    it("should return notAuthorized for empty authorization.", async () => {
         const sut = new AuthMiddleware();
         const middlewareResponse = await sut.handle({ Authorization: "" });
-        expect(middlewareResponse).toEqual(httpResponse.notAuthorized())
+        expect(middlewareResponse).toEqual(httpResponse.notAuthorized());
+    });
+
+    it("should return notAuthorized for authorization that hasnt Bearer prefix.", async () => {
+        const sut = new AuthMiddleware();
+        const middlewareResponse = await sut.handle({ Authorization: "any_token" });
+        expect(middlewareResponse).toEqual(httpResponse.notAuthorized());
     });
 });
