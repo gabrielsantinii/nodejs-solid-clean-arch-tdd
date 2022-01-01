f1d7b537fc94dd625237bcb7228d915fef322d22
Gabriel Santini <gabrielgsantini@gmail.com>
2022-01-01 18:59:47 -0300

    test: load-profile-by-auth-token and load-profile usecases added as dependency to auth-middleware

--- src/presentation/middlewares/auth-middleware/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware/auth-middleware.spec.ts
@@ -1,7 +1,33 @@
+import { LoadProfile } from "@/domain/usecases";
 import { httpResponse } from "@/presentation/helpers";
 import { HttpResponse, Middleware } from "@/presentation/protocols";
 
+class LoadProfileSpy implements LoadProfile {
+    result: LoadProfile.Result = undefined;
+    async perform(params: LoadProfile.Params): Promise<LoadProfile.Result> {
+        return this.result;
+    }
+}
+
+class LoadProfileIdByAuthTokenSpy implements LoadProfileIdByAuthToken {
+    result: LoadProfileIdByAuthToken.Result = undefined;
+    async perform(params: LoadProfileIdByAuthToken.Params): Promise<LoadProfileIdByAuthToken.Result> {
+        return this.result;
+    }
+}
+
+interface LoadProfileIdByAuthToken {
+    perform: (params: LoadProfileIdByAuthToken.Params) => Promise<LoadProfileIdByAuthToken.Result>;
+}
+
+namespace LoadProfileIdByAuthToken {
+    export type Params = { authToken: string };
+    export type Result = { profileId: string } | undefined;
+}
+
 class AuthMiddleware implements Middleware {
+    constructor(private readonly loadProfileIdByAuthToken: LoadProfileIdByAuthToken, private readonly loadProfile: LoadProfile) {}
+
     async handle(request: { Authorization: string }): Promise<HttpResponse> {
         const authorization = request?.Authorization;
         if (!authorization) return httpResponse.notAuthorized();
@@ -19,21 +45,34 @@ class AuthMiddleware implements Middleware {
     }
 }
 
+type SutType = {
+    sut: AuthMiddleware;
+    loadProfileIdByAuthTokenSpy: LoadProfileIdByAuthTokenSpy;
+    loadProfileSpy: LoadProfileSpy;
+};
+
+export const makeSut = (): SutType => {
+    const loadProfileIdByAuthTokenSpy = new LoadProfileIdByAuthTokenSpy();
+    const loadProfileSpy = new LoadProfileSpy();
+    const sut = new AuthMiddleware(loadProfileIdByAuthTokenSpy, loadProfileSpy);
+    return { sut, loadProfileSpy, loadProfileIdByAuthTokenSpy };
+};
+
 describe("auth-middleware.spec usecase", () => {
     it("should return notAuthorized for empty authorization.", async () => {
-        const sut = new AuthMiddleware();
+        const { sut } = makeSut();
         const middlewareResponse = await sut.handle({ Authorization: "" });
         expect(middlewareResponse).toEqual(httpResponse.notAuthorized());
     });
 
     it("should return notAuthorized for authorization that hasnt Bearer prefix.", async () => {
-        const sut = new AuthMiddleware();
+        const { sut } = makeSut();
         const middlewareResponse = await sut.handle({ Authorization: "any_token" });
         expect(middlewareResponse).toEqual(httpResponse.notAuthorized());
     });
 
     it("should return notAuthorized for authorization that has more than 2 words (prefix and token).", async () => {
-        const sut = new AuthMiddleware();
+        const { sut } = makeSut();
         const middlewareResponse = await sut.handle({ Authorization: "Bearer anytoken invalidWord" });
         expect(middlewareResponse).toEqual(httpResponse.notAuthorized());
     });
