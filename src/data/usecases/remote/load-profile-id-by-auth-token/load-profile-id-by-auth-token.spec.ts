c9a47a0c702e197873b1d3a55b434e4d56c149db
Gabriel Santini <gabrielgsantini@gmail.com>
2022-01-01 19:29:23 -0300

    test: load-profile-by-id-adapter interface created for dependency, spy created for tests

--- src/data/usecases/remote/load-profile-id-by-auth-token/load-profile-id-by-auth-token.spec.ts
+++ src/data/usecases/remote/load-profile-id-by-auth-token/load-profile-id-by-auth-token.spec.ts
@@ -1,6 +1,24 @@
 import { LoadProfileIdByAuthToken } from "@/domain/usecases";
 
+export interface LoadProfileIdByAuthTokenAdapter {
+    loadAuthIdByToken: (params: LoadProfileIdByAuthTokenAdapter.Params) => Promise<LoadProfileIdByAuthTokenAdapter.Result>;
+}
+
+export namespace LoadProfileIdByAuthTokenAdapter {
+    export type Params = { token: string };
+    export type Result = { authId: string };
+}
+
+class LoadProfileIdByAuthTokenAdapterSpy implements LoadProfileIdByAuthTokenAdapter {
+    result: LoadProfileIdByAuthTokenAdapter.Result = { authId: undefined };
+    async loadAuthIdByToken(params: LoadProfileIdByAuthTokenAdapter.Params): Promise<LoadProfileIdByAuthTokenAdapter.Result> {
+        return this.result;
+    }
+}
+
 class RemoteLoadProfileIdByAuthToken implements LoadProfileIdByAuthToken {
+    constructor(private readonly loadProfileIdByAuthTokenAdapter: LoadProfileIdByAuthTokenAdapter) {}
+
     async perform(params: LoadProfileIdByAuthToken.Params): Promise<LoadProfileIdByAuthToken.Result> {
         return { profileId: "any-profile-id" };
     }
@@ -8,7 +26,8 @@ class RemoteLoadProfileIdByAuthToken implements LoadProfileIdByAuthToken {
 
 describe("load-profile-id-by-auth-token.spec usecase", () => {
     it("should return profileId for valid auth-token.", async () => {
-        const sut = new RemoteLoadProfileIdByAuthToken();
+        const loadProfileIdByAuthTokenAdapterSpy = new LoadProfileIdByAuthTokenAdapterSpy()
+        const sut = new RemoteLoadProfileIdByAuthToken(loadProfileIdByAuthTokenAdapterSpy);
         const profileByAuthToken = await sut.perform({ authToken: "any_token" });
         expect(profileByAuthToken).toHaveProperty("profileId");
     });
