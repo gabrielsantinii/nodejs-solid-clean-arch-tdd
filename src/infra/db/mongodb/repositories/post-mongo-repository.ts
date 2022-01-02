c9b387d6f4a5ae3e3f2bbd63478ae283a6f04ecd
Gabriel Santini <gabrielgsantini@gmail.com>
2022-01-02 18:02:53 -0300

    feat: add-post implemented on post-mongo-repository infrastructure

--- src/infra/db/mongodb/repositories/post-mongo-repository.ts
+++ src/infra/db/mongodb/repositories/post-mongo-repository.ts
@@ -1,13 +1,20 @@
 import mongoose from "mongoose";
-import { LoadPostsByAuthorRepository, LoadRecentPostsRepository } from "@/data/protocols/db";
+import { AddPostRepository, LoadPostsByAuthorRepository, LoadRecentPostsRepository } from "@/data/protocols/db";
 import { MongoHelper } from "@/infra/db/mongodb/helpers";
 import { PostModel } from "@/domain/models";
 
-export class PostMongoRepository implements LoadRecentPostsRepository, LoadPostsByAuthorRepository {
+export class PostMongoRepository implements LoadRecentPostsRepository, LoadPostsByAuthorRepository, AddPostRepository {
     private readonly collection: mongoose.Model<PostModel>;
     constructor() {
         this.collection = MongoHelper.getCollection("posts");
     }
+
+    async add(params: AddPostRepository.Params): Promise<AddPostRepository.Result> {
+        const createdPost = new this.collection({ ...params, postedAt: new Date() });
+        await createdPost.save();
+        return createdPost.toJSON() as any;
+    }
+
     async loadWithLimit(params: LoadRecentPostsRepository.Params): Promise<LoadRecentPostsRepository.Result> {
         const posts = await this.collection.find({ authorId: { $in: params.authorsIds } }).limit(params.limit);
         return posts.map((p) => p.toJSON()) as any;
