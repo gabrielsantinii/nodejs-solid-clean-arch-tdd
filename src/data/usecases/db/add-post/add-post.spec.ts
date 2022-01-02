ef887d6b577eb8b1d765f48211a4b483a7c00893
Gabriel Santini <gabrielgsantini@gmail.com>
2022-01-02 17:54:01 -0300

    test: ensure reject on missing id of saved post using spy

--- src/data/usecases/db/add-post/add-post.spec.ts
+++ src/data/usecases/db/add-post/add-post.spec.ts
@@ -2,27 +2,77 @@ import { PostModel } from "@/domain/models";
 import { mockPostModel } from "@/tests/domain/mocks";
 import faker from "faker";
 
-class AddPost {
-    async perform(params: {
-        contentDescription: string;
-        postedBy: { id: string; name: string; username: string; avatarUrl: string };
-    }): Promise<PostModel> {
-        return mockPostModel();
+const mockAddPostParams = (): AddPost.Params => ({
+    contentDescription: faker.lorem.lines(3),
+    postedBy: {
+        avatarUrl: "any_avatar",
+        id: faker.datatype.uuid(),
+        name: "any_name",
+        username: "any_username",
+    },
+});
+
+interface AddPostRepository {
+    add: (params: AddPostRepository.Params) => Promise<AddPostRepository.Result>;
+}
+
+namespace AddPostRepository {
+    export type Params = AddPost.Params;
+    export type Result = AddPost.Result;
+}
+
+class AddPostRepositorySpy implements AddPostRepository {
+    result: AddPostRepository.Result = mockPostModel();
+    async add(params: AddPostRepository.Params): Promise<AddPostRepository.Result> {
+        return this.result;
     }
 }
 
+export class AddPost {
+    constructor(private readonly addPostRepository: AddPostRepository) {}
+    
+    async perform(params: AddPost.Params): Promise<AddPost.Result> {
+        const savedPost = await this.addPostRepository.add(params)
+        if (!savedPost?.id) throw new Error('Could not add post')
+        return savedPost
+    }
+}
+
+export namespace AddPost {
+    export type Params = {
+        contentDescription: string;
+        postedBy: {
+            id: string;
+            name: string;
+            username: string;
+            avatarUrl: string;
+        };
+    };
+    export type Result = PostModel;
+}
+
+const makeSut = () => {
+    const addPostRepositorySpy = new AddPostRepositorySpy();
+    const sut = new AddPost(addPostRepositorySpy);
+    return { sut, addPostRepositorySpy };
+};
+
 describe("add-post.spec usecase", () => {
     it("should return the id of saved post.", async () => {
-        const sut = new AddPost();
-        const savedPost = await sut.perform({
-            contentDescription: faker.lorem.lines(3),
-            postedBy: {
-                avatarUrl: "any_avatar",
-                id: faker.datatype.uuid(),
-                name: "any_name",
-                username: "any_username",
-            },
-        });
-        expect(savedPost.id).toBeTruthy()
+        const { sut } = makeSut();
+        const savedPost = await sut.perform(mockAddPostParams());
+        expect(savedPost.id).toBeTruthy();
+    });
+
+    it("should reject on missing id of saved post.", async () => {
+        const { sut, addPostRepositorySpy } = makeSut();
+        let error: Error;
+        try {
+            addPostRepositorySpy.result = undefined
+            await sut.perform(mockAddPostParams());
+        } catch (err) {
+            error = err;
+        }
+        expect(error).toBeTruthy();
     });
 });
